04337240e7728fdeaa894e35f4864bb8f1a0f7e1
Israfilovrus@gmail.com <Ruslan>
2022-11-11 14:50:04 +0300

    Определение класса отдельной команды

---
+++ PR22/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,21 @@
+﻿using PR22.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PR22.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+ 
+    }
+}
