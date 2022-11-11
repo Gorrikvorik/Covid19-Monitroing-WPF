12e82da25716ee6e1f0f1f2dc56e41e7c47d8176
Israfilovrus@gmail.com <Ruslan>
2022-11-11 14:33:30 +0300

    Главный класс команды для приложения

---
+++ PR22/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using PR22.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR22.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object,bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+        public override bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => execute(parameter);
+        
+    }
+}
