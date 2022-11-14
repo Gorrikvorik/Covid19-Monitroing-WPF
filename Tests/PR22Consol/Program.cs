5c23e59064507bbea7c0d3e9b717b5ba6b687fb2
Israfilovrus@gmail.com <Ruslan>
2022-11-14 09:41:54 +0300

    Извлечение данных по датам записей

--- Tests/PR22Consol/Program.cs
+++ Tests/PR22Consol/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
 
-const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+class Program {
+    private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static async Task<Stream> GetDataStream()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+        return await response.Content.ReadAsStreamAsync();
+    }
+
+  private static  IEnumerable<String> GetDataLines()
+    {
+        using var data_stream =   GetDataStream().Result;
+        using var data_reader =  new StreamReader(data_stream);
+
+      while (!data_reader.EndOfStream)
+        {
+            var line =  data_reader.ReadLine();
+            if (string.IsNullOrEmpty(line)) continue;
+            yield return line;
+        }
+    }
+
+    private static DateTime[] GetDates() => GetDataLines()
+        .First()
+        .Split(',')
+        .Skip(4)
+        .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+        .ToArray();
 
 
-var client = new HttpClient();
-var response = client.GetAsync(data_url).Result;
-var csv_str_string = response.Content.ReadAsStringAsync().Result;
+    static void Main()
+    {
+        var dates = GetDates();
+        Console.WriteLine(string.Join("\r\n", dates));
+        Console.ReadLine();
+    }
 
-Console.ReadLine();
+   
+}
