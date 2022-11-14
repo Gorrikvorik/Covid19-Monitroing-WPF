752bc181cf4792de6fc392cc4774460dc9bb5087
Israfilovrus@gmail.com <Ruslan>
2022-11-14 10:21:03 +0300

    Тестирование бизнес-логики

--- Tests/PR22Consol/Program.cs
+++ Tests/PR22Consol/Program.cs
@@ -26,7 +26,7 @@ class Program {
         {
             var line =  data_reader.ReadLine();
             if (string.IsNullOrEmpty(line)) continue;
-            yield return line;
+            yield return line.Replace("Korea,","Korea -");
         }
     }
 
@@ -37,11 +37,28 @@ class Program {
         .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
         .ToArray();
 
+    private static IEnumerable<(string Country,string Province, int[] Counts)> GetData()
+    {
+        var lines = GetDataLines()
+            .Skip(1)
+            .Select(line => line.Split(','));
 
+        foreach(var row in lines)
+        {
+            var province = row[0].Trim();
+            var country_name = row[1].Trim(' ', '"');
+            var counts = row.Skip(5)
+                .Select( s => int.Parse(s))
+                .ToArray();
+            yield return(country_name, province, counts);
+        }
+
+    }
     static void Main()
     {
-        var dates = GetDates();
-        Console.WriteLine(string.Join("\r\n", dates));
+        var russia_data = GetData()
+            .First(v => v.Country.Equals("Russia",StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts,(data,count) =>$"{data:dd:MM} - {count}")));
         Console.ReadLine();
     }
 
