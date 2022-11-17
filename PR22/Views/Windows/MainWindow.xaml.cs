9010d591ecd0b9411a9fa58715aa5f9efb892c1c
Israfilovrus@gmail.com <Ruslan>
2022-11-17 13:11:43 +0300

    Восстановили работоспособность

--- PR22/Views/Windows/MainWindow.xaml.cs
+++ PR22/Views/Windows/MainWindow.xaml.cs
@@ -5,6 +5,6 @@ namespace PR22
    
     public partial class MainWindow 
     {
-        public MainWindow() => InitializeComponent(); fds ds
+        public MainWindow() => InitializeComponent();
     }
 }
