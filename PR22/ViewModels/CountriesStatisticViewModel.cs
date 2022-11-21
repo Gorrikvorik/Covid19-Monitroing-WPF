72e943c18f5c25cb41a45cf5ac4832219eb576cb
Israfilovrus@gmail.com <Ruslan>
2022-11-21 10:59:20 +0300

    Базовая логика модели представления статистики

--- PR22/ViewModels/CountriesStatisticViewModel.cs
+++ PR22/ViewModels/CountriesStatisticViewModel.cs
@@ -1,10 +1,13 @@
-﻿using PR22.Services;
+﻿using PR22.Infrastructure.Commands;
+using PR22.Models;
+using PR22.Services;
 using PR22.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PR22.ViewModels
 {
@@ -16,11 +19,39 @@ namespace PR22.ViewModels
 
         private MainWindowViewModel MainModel { get;}
 
+
+
+        #region Countries : Ienumerable<CountryInfo> - Статистика по странам
+
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+
+        #region Команды
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = _DataSerive.GetData();
+        }
+        #endregion
+
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
-             MainModel = MainModel;
+             this.MainModel = MainModel;
 
             _DataSerive = new DataService();
+
+            #region Команды
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+            #endregion
         }
     }
 }
