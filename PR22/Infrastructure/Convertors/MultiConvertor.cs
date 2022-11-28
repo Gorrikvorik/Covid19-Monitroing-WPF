93cac16edc1f4329898231a856e9ef57518b6a58
RuslanI <Ruslan>
2022-11-28 10:48:01 +0300

    Мульти Конвертор

---
+++ PR22/Infrastructure/Convertors/MultiConvertor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PR22.Infrastructure.Convertors
+{
+    internal abstract class MultiConvertor : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => 
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
