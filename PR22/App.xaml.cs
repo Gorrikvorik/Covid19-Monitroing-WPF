65f7d2b6e5d8657e671a25c48241f352566e8afd
RuslanI <Ruslan>
2022-11-30 10:54:40 +0300

    Запуск хоста и остановка его при входе/выходе приложения

--- PR22/App.xaml.cs
+++ PR22/App.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using PR22.Services;
 using PR22.ViewModels;
+using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
@@ -13,14 +14,32 @@ namespace PR22
     {
         public static bool IsDesignModel { get; private set; } = true;
 
-        
 
-        protected override void OnStartup(StartupEventArgs e)
+        private static IHost _Host;
+
+        public static IHost Host => _Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesignModel = false;
+            var host = Host;
             base.OnStartup(e);
+
+          await  host.StartAsync().ConfigureAwait(false);
+
                  
  
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+
+            host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _Host = null;
         }
 
         public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
