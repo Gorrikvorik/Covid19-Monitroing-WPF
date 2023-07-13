6064109c79d5bcc7f1dd6cd51e46071d9b967f0a
RuslanI <Ruslan>
2023-07-13 13:50:27 +0300

    Заготовка асинхронного сервера

--- PR22.Web/WebServer.cs
+++ PR22.Web/WebServer.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace PR22.Web
 {
     internal class WebServer
     {
+        //  private readonly TcpListener _Listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080)); работает с драйвером сетеовй карты
+        private HttpListener _Listener; // работает с операционной системой ( меньше возможностей, но проще реализовать)
+        private readonly int _Port;
+        private bool _Enabled;
+        private  readonly object _SyncRoot = new object();
+
+        public int Port => _Port;
+        public bool Enabled { get => _Enabled; set
+            {
+                if (value) Start(); else Stop();
+            }
+        }
+        public WebServer(int Port) => _Port = Port;
+
+        public void Start() 
+        {
+            if (_Enabled) return;
+            lock(_SyncRoot)
+            {
+                if (_Enabled) return;
+                _Listener = new HttpListener();
+                _Listener.Prefixes.Add($"http://*:{ _Port}");
+                _Listener.Prefixes.Add($"http://+:{ _Port}");
+                _Enabled = true;
+          
+
+            }
+            Listen();
+        }
+
+        public void Stop() 
+        {
+            if(!_Enabled) return;
+            lock(_SyncRoot)
+            {  
+                if (!_Enabled) return;
+                _Listener = null;
+                _Enabled = false;
+            }
+
+        }
+
+        private void Listen()
+        {
+
+        }
     }
 }
