6d47bad5deedafe14477391dbb49273aba73bbf1
RuslanI <Ruslan>
2023-07-17 10:51:32 +0300

    Добавлены поведения для системных кнопок

---
+++ PR22/Infrastructure/Behaviors/MinimizeWindow.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PR22.Infrastructure.Behaviors
+{
+    class MinimizeWindow :Behavior<Button>
+    {
+        protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+
+        protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+         ;
+            if (!(AssociatedObject.FindVisualRoot() is  Window window)) return;
+            //switch(window.WindowState)
+            //{
+            //    case WindowState.Normal:
+            //        window.WindowState = WindowState.Maximized;
+            //        break;
+            //    case WindowState.Maximized:
+            //        window.WindowState = WindowState.Normal;
+            //        break;
+
+            //}
+            window.WindowState = window.WindowState switch
+            {
+                WindowState.Normal => WindowState.Maximized,
+                WindowState.Maximized => WindowState.Normal,
+                _ => window.WindowState
+            };
+        }
+    }
+}
--- PR22/Infrastructure/Behaviors/MinimizeWindowChange.cs
+++ PR22/Infrastructure/Behaviors/MinimizeWindowChange.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.Xaml.Behaviors;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace PR22.Infrastructure.Behaviors
 {
-    internal class CloseWindow : Behavior<Button>
+    internal class MinimizeWindowChange : Behavior<Button>
     {
         protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
 
         protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
 
-        private void OnButtonClick(object sender, RoutedEventArgs e) => 
-            (AssociatedObject.FindVisualRoot() as Window)?.Close();
-
-
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            ;
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+ 
+            window.WindowState = WindowState.Minimized;
+            
+        }
     }
 }
