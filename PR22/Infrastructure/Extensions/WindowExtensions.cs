e42a53639b10a63dd610d55b3305976b6bf1397a
RuslanI <Ruslan>
2023-07-17 15:00:16 +0300

    Добавили логику масштабирования окна

--- PR22/Infrastructure/Extensions/WindowExtensions.cs
+++ PR22/Infrastructure/Extensions/WindowExtensions.cs
@@ -11,11 +11,21 @@ namespace System.Windows
         private const string user32 = "user32.dll";
 
         [DllImport(user32, CharSet =CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hwnd,uint Msg,IntPtr wParam,IntPtr lParam);
-        public static IntPtr SendMessage(this Window window,WM  Msg,SC  wParam,IntPtr lParam = default)
+        private static extern IntPtr SendMessage(IntPtr hWnd,uint Msg,IntPtr wParam,IntPtr lParam);
+        [DllImport(user32, CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd,WM Msg,IntPtr wParam,IntPtr lParam);
+        public static IntPtr SendMessage(this Window window, WM Msg, IntPtr wParam, IntPtr lParam = default)
         {
-            return SendMessage(window.GetWindowHandle(), (uint)Msg, (IntPtr)wParam, lParam == default ? (IntPtr)' ': lParam);
+            return SendMessage(window.GetWindowHandle(), Msg,  wParam, lParam);
+        }
 
+        public static IntPtr SendMessage(this Window window, WM Msg, SC wParam, IntPtr lParam = default)
+        {
+            return SendMessage(
+                window.GetWindowHandle(), 
+                Msg: (uint)Msg, 
+                (IntPtr)wParam,
+                lParam == default ? (IntPtr)' ' : lParam);
         }
 
         private static IntPtr GetWindowHandle(this Window window)
